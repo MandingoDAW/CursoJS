6b65c4f73ece00bb7d72adca284e97b0f2695c56
Eric <epmendez94@gmail.com>
2018-08-01 23:43:07 +0200

    Ejercicio eventos casi finalizado

--- js/Ejercicios-DOM-Eventos-Funciones.js
+++ js/Ejercicios-DOM-Eventos-Funciones.js
@@ -4,7 +4,39 @@ window.addEventListener('load', function(){
 
 	console.log("DOM cargado!");
 
+	
+		var formulario = document.getElementById("formulario");
+		
+
+		console.log(formulario);
+
+		formulario.addEventListener('submit', function(){
+
+			console.log("Submit");
+
+			var nombre = document.querySelector("#nombre").value;
+			var apellidos = document.querySelector("#apellidos").value;
+			var edad = document.querySelector("#edad").value;
+			
+			console.log(nombre);
+			console.log(apellidos);
+			console.log(edad);
+
+			var divDatos = document.createElement("div");
+			
+			
+			document.body.appendChild(divDatos);
+			divDatos.style.borderStyle = "dashed";
+			//
+
+			divDatos.style.marginTop = "50px";
+			divDatos.appendChild(datosForm);
 
 
+
+		});
+	
+
+	
 
 });
