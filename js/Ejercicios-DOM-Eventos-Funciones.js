15d219845530f2113bbce5f829491f8658f13421
eric <epmendez94@gmail.com>
2018-08-02 13:18:54 +0200

    Ejercicios eventos finalizados

--- js/Ejercicios-DOM-Eventos-Funciones.js
+++ js/Ejercicios-DOM-Eventos-Funciones.js
@@ -6,6 +6,9 @@ window.addEventListener('load', function(){
 
 	
 		var formulario = document.getElementById("formulario");
+		formulario.style.width = "20%";
+		formulario.style.borderStyle = "double";
+		formulario.textAlign = "center";
 		
 
 		console.log(formulario);
@@ -17,6 +20,25 @@ window.addEventListener('load', function(){
 			var nombre = document.querySelector("#nombre").value;
 			var apellidos = document.querySelector("#apellidos").value;
 			var edad = document.querySelector("#edad").value;
+
+			if(nombre.trim() == null || nombre.trim().length == 0){
+				alert("Nombre no válido");
+				return false;
+
+			}
+
+			if(apellidos.trim() == null || apellidos.trim().length == 0){
+				alert("Apellidos no válido");
+				return false;
+
+			}
+
+			if(edad.trim() == null || edad.trim().length == 0 || edad.trim() == isNaN(edad)){
+				alert("Edad no válida");
+				return false;
+
+			}
+
 			
 			console.log(nombre);
 			console.log(apellidos);
@@ -26,11 +48,20 @@ window.addEventListener('load', function(){
 			
 			
 			document.body.appendChild(divDatos);
-			divDatos.style.borderStyle = "dashed";
-			//
+			
 
 			divDatos.style.marginTop = "50px";
-			divDatos.appendChild(datosForm);
+			divDatos.innerHTML = "<h3>Datos del formulario</h3><br><p>Nombre: "+nombre+"</p><br><p>Apellidos: "+apellidos+"</p><br><p>Edad: "+edad+"</p>";
+			divDatos.style.width = "15%";
+			divDatos.style.float = "right";
+			divDatos.style.marginTop = "-16.5%";
+			divDatos.style.textAlign = "center";
+			divDatos.style.marginRight = "60%";
+			divDatos.style.borderStyle = "dashed";
+
+
+			
+
 
 
 
